f0fecc4d8906e78ad416a2fb31d5ebf2a62af7c3
RanyellHenrique <ranyellhenrique@gmail.com>
2019-08-10 17:35:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
+		System.out.println("======= TEST 5 : seller Insert ========");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
 	}
 
